4d9821cb9d58ee23c6497747c960189fa30bc0f7
viiper94 <viiper94@gmail.com>
2018-01-15 10:48:37 +0200

    add favicon

--- modules/header.php
+++ modules/header.php
@@ -4,11 +4,12 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link type="text/css" rel="stylesheet" href="assets/materialize/css/materialize.min.css"  media="screen,projection"/>
 	<link type="text/css" rel="stylesheet" href="assets/select2/select2.min.css"  media="screen,projection"/>
 	<link type="text/css" rel="stylesheet" href="assets/select2/select2-custom.css"  media="screen,projection"/>
-	<link type="text/css" rel="stylesheet" href="assets/css/style.css"  media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="assets/css/style.css?1501"  media="screen,projection"/>
 	<!-- Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-99570317-2"></script>
 	<script>
@@ -22,5 +23,5 @@
 	<title><?= t('title') ?></title>
 </head>
 <body class="grey darken-4">
-	<?php require_once 'navbar.php'; ?>
-	<main>
+<?php require_once 'navbar.php'; ?>
+<main>
