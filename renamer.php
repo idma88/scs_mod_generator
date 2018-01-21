b2cf191b6725a60ec70be74a0d2ff028b5e4e26a
viiper94 <viiper94@gmail.com>
2018-01-21 12:18:25 +0200

    detect user os and browser

--- renamer.php
+++ renamer.php
@@ -91,9 +91,11 @@
 	$filename = zip_files($title);
 
 	$log = file_get_contents('log.txt');
+	$user_data = getBrowser();
 	$log .= date('d-m-Y H:i')." $filename\nTitle:\t\t$_POST[title]\nChassis:\t$_POST[chassis]\n".
 		"Accessory:\t$_POST[accessory]\nPaint:\t\t$_POST[paint]\nWeight:\t\t$_POST[weight]".
-		"\nColor:\t\t".$_POST['color']['scs']['r'].", ".$_POST['color']['scs']['g'].", ".$_POST['color']['scs']['b']."\nTarget:\t\t$_POST[target]\n\n";
+		"\nColor:\t\t".$_POST['color']['scs']['r'].", ".$_POST['color']['scs']['g'].", ".$_POST['color']['scs']['b']."\n".
+		"Target:\t\t$_POST[target]\nUser:\t\t".$user_data['platform']." ".$user_data['name']." ".$user_data['version']."\n\n";
 	file_put_contents('log.txt', $log);
 
 	$add = $_POST['target'] == 'ats' ? 'game=ats&' : '';
