89b202410c34eeed60cfa0211757c5717e5c89da
viiper94 <viiper94@gmail.com>
2018-01-30 02:58:50 +0200

    implement some OOP

---
+++ classes/Chassis.php
@@ -0,0 +1,84 @@
+<?php
+
+class Chassis{
+
+	public $chassis_name;
+	public $chassis_def;
+	public $default_paint_job;
+	public $wheels;
+	public $customWheels = false;
+	public $axles;
+	public $weight = false;
+	public $dlc = array();
+	public $game;
+
+	public function __construct(){
+		GLOBAL $chassis_list, $axles, $with_paint_job;
+		$this->chassis_name = $_POST['chassis'];
+		$this->game = $_POST['target'];
+		$this->chassis_def = $chassis_list[$this->game][$this->chassis_name];
+		$this->wheels = $this->getWheels();
+		$this->axles = $axles[$this->chassis_name];
+		$this->default_paint_job = $this->isWithPaintJob() ? $with_paint_job[$this->chassis_name] : null;
+		$this->dlc = $this->getChassisDlc();
+		$this->weight = isset($_POST['weight']) && is_numeric($_POST['weight']) ? $_POST['weight'] : false;
+	}
+
+	private function getWheels(){
+		GLOBAL $chassis_one_wheel_support, $wheels;
+		if(in_array($this->chassis_name, $chassis_one_wheel_support) || !isset($_POST['wheels']) || $_POST['wheels'] == ''){
+			$wheels = $wheels[$this->chassis_name];
+		}else{
+			$this->customWheels = true;
+			$wheels = $_POST['wheels'];
+		}
+		return $wheels;
+	}
+
+	private function getChassisDlc(){
+		GLOBAL $dlc_chassis_list;
+		$dlc = $this->dlc;
+		if(key_exists($this->chassis_name, $dlc_chassis_list)){
+			$dlc = explode(',', $dlc_chassis_list[$this->chassis_name]);
+		}
+		return $dlc;
+	}
+
+	public function isWithPaintJob(){
+		GLOBAL $with_paint_job;
+		return key_exists($this->chassis_name, $with_paint_job);
+	}
+
+	public function isWithAccessory(){
+		GLOBAL $with_accessory;
+		return in_array($this->chassis_name, $with_accessory);
+	}
+
+	public function getAvailableAccessories($lang = null){
+		GLOBAL $accessories;
+		$list = array();
+		if($this->isWithAccessory()){
+			$chassis = str_replace(['_default', '_black', '_yellow', '_red', '_blue', '_grey'], '', $this->chassis_name);
+			foreach($accessories[$this->game][$chassis] as $def => $name){
+				$list[$def] = t($name, $lang);
+			}
+			return $list;
+		}else{
+			return false;
+		}
+	}
+
+	public function getAvailablePaints($lang = null){
+		GLOBAL $paints;
+		if($this->isWithPaintJob()){
+			$chassis = str_replace(['_1', '_1_4', '_4', '_4_3', 'rm_double', 'rm53_double', 'pup_double', 'pup_triple'], '', $this->chassis_name);
+			foreach($paints[$this->game][$chassis] as $def){
+				$list[$def] = t(PaintJob::getTrailerLookByDef($def), $lang);
+			}
+			return $list;
+		}else{
+			return false;
+		}
+	}
+
+}
---
+++ generator.php
@@ -0,0 +1,55 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+	header('Location: http://'.$_SERVER['HTTP_HOST']);
+	exit;
+}
+
+include 'functions.php';
+include 'arrays.php';
+include 'classes/kint/Kint.class.php';
+include 'classes/App.php';
+include 'classes/Chassis.php';
+include 'classes/Accessory.php';
+include 'classes/PaintJob.php';
+include 'classes/Image.php';
+
+// POST validation
+if(!isset($_POST['chassis']) || $_POST['chassis'] == ''){
+	header('Location: http://'.$_SERVER['HTTP_HOST'].'/?e=2301');
+}
+if(in_array($_POST['chassis'], $with_accessory) && !isset($_POST['accessory'])){
+	header('Location: http://'.$_SERVER['HTTP_HOST'].'/?e=9502');
+}
+if(key_exists($_POST['chassis'], $with_paint_job) && (!isset($_POST['paint']) || $_POST['paint'] == '')){
+	header('Location: http://'.$_SERVER['HTTP_HOST'].'/?e=8113');
+}
+if(!isset($_POST['target']) || $_POST['target'] == ''){
+	header('Location: http://'.$_SERVER['HTTP_HOST'].'/?e=6764');
+}
+
+$chassis = new Chassis();
+$accessory = null;
+$paint_job = null;
+
+if($chassis->isWithAccessory()) $accessory = new Accessory();
+if($chassis->isWithPaintJob() || $chassis->chassis_name == 'aero_dynamic') $paint_job = new PaintJob($chassis);
+
+//!d($paint_job); exit;
+
+$generator = new App();
+$generator->load($chassis, $accessory, $paint_job);
+$generator->run();
+
+$log = file_get_contents('log.txt');
+$user_data = getBrowser();
+$append = date('d-m-Y H:i')." $generator->fileName\nTitle:\t\t$_POST[title]\nChassis:\t$_POST[chassis]\n".
+	"Accessory:\t$_POST[accessory]\nPaint:\t\t$_POST[paint]\nWeight:\t\t$_POST[weight]".
+	"\nColor:\t\t".$_POST['color']['scs']['r'].", ".$_POST['color']['scs']['g'].", ".$_POST['color']['scs']['b']."\n".
+	"Wheels: \t$_POST[wheels]\n".
+	"Target:\t\t$_POST[target]\nUser:\t\t".$user_data['platform']." ".$user_data['name']." ".$user_data['version']."\n\n";
+file_put_contents('log.txt', $append.$log);
+
+$add = $_POST['target'] == 'ats' ? 'game=ats&' : '';
+
+header('Location: http://'.$_SERVER['HTTP_HOST'].'/?'.$add.'d='.$generator->fileName);
