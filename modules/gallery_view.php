c5a7eefc52ea52dac22f5a695077d363899b4d2e
viiper94 <viiper94@gmail.com>
2018-02-13 18:22:16 +0200

    add dark/light theme feature

--- modules/gallery_view.php
+++ modules/gallery_view.php
@@ -1,9 +1,9 @@
 <?php GLOBAL $chassis_list, $with_paint_job, $with_accessory; ?>
-<div class="container grey-text">
+<div class="container">
 	<div class="row">
-		<ul class="tabs grey darken-4">
-			<li class="tab col s6"><a class="active grey-text" href="#ets2">Euro Truck Simulator 2</a></li>
-			<li class="tab col s6"><a href="#ats" class="grey-text">American Truck Simulator</a></li>
+		<ul class="tabs">
+			<li class="tab col s6"><a class="active waves-effect" href="#ets2">Euro Truck Simulator 2</a></li>
+			<li class="tab col s6"><a href="#ats" class="waves-effect">American Truck Simulator</a></li>
 		</ul>
 		<div id="ets2" class="game">
 			<h4 class="light"><?= t('ets_trailer_guide') ?></h4>
@@ -19,17 +19,17 @@
 			];
 			foreach(array_values(array_unique($chassis)) as $key => $name) :?>
 				<div class="col m6 s12">
-					<div class="card trailer grey darken-3 <?= $key ?>">
+					<div class="card trailer <?= $key ?>">
 						<div class="card-image">
 							<img src="/assets/img/trailers/<?= $name ?>/<?= $name ?>.jpg">
-							<h5 class="card-title trailer-name light text-shadow"><?= t($name) ?></h5>
+							<h5 class="card-title trailer-name text-shadow"><?= t($name) ?></h5>
 						</div>
 						<?php $name_alt = in_array($name, $diff) ? $name . '_default' : $name;
 						if(key_exists($name_alt, $with_paint_job) || in_array($name_alt, $with_accessory)) : ?>
 							<div class="card-content">
-								<ul class="collapsible show-skin grey darken-3 z-depth-0 grey-text text-lighten-2" data-trailer="<?= $name_alt ?>" data-game="ets2">
+								<ul class="collapsible show-skin z-depth-0" data-trailer="<?= $name_alt ?>" data-game="ets2">
 									<li>
-										<div class="collapsible-header grey darken-3">
+										<div class="collapsible-header">
 											<i class="material-icons notranslate">arrow_downward</i>
 											<?php if(key_exists($name_alt, $with_paint_job)) : ?>
 												<span style="flex: 1;"><?= t('see_paints') ?></span>
@@ -64,17 +64,17 @@
 			];
 			foreach(array_values(array_unique($chassis)) as $key => $name) :?>
 				<div class="col m6 s12">
-					<div class="card trailer grey darken-3 <?= $key ?>">
+					<div class="card trailer <?= $key ?>">
 						<div class="card-image">
 							<img src="/assets/img/trailers/<?= $name ?>/<?= $name ?>.jpg">
-							<h5 class="card-title trailer-name light text-shadow"><?= t($name) ?></h5>
+							<h5 class="card-title trailer-name text-shadow"><?= t($name) ?></h5>
 						</div>
 						<?php $name_alt = key_exists($name, $diff) ? $diff[$name] : $name;
 							if(key_exists($name_alt, $with_paint_job) || in_array($name_alt, $with_accessory)) : ?>
 								<div class="card-content">
-									<ul class="collapsible show-skin grey darken-3 z-depth-0 grey-text text-lighten-2" data-trailer="<?= $name_alt ?>" data-game="ats">
+									<ul class="collapsible show-skin z-depth-0" data-trailer="<?= $name_alt ?>" data-game="ats">
 										<li>
-											<div class="collapsible-header grey darken-3">
+											<div class="collapsible-header">
 												<i class="material-icons notranslate">arrow_downward</i>
 												<?php if(key_exists($name_alt, $with_paint_job)) : ?>
 													<span style="flex: 1;"><?= t('see_paints') ?></span>
