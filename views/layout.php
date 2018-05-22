02ae1a0ccd0c3f23293f3c5db3d36800b39179e0
viiper94 <viiper94@gmail.com>
2018-05-22 14:19:09 +0300

    fix layout paths

--- views/layout.php
+++ views/layout.php
@@ -1,18 +1,18 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<?php include_once 'header.php'; ?>
+	<?php include_once __DIR__.'/header.php'; ?>
 </head>
 <body<?php if(isset($_COOKIE['dark_theme']) && $_COOKIE['dark_theme'] == 'true'): ?> class="mdc-theme--dark"<?php endif ?>>
-	<?php include_once 'navbar.php'; ?>
-	<main>
-		<?php if(stripos($_SERVER['SCRIPT_NAME'], 'gallery')){
-			include_once 'gallery.php';
-		}else{
-			include_once 'index.php';
-		} ?>
-		<?php include_once 'lang_modal.php' ?>
-	</main>
-	<?php include_once 'footer.php'; ?>
+<?php include_once __DIR__.'/navbar.php'; ?>
+<main>
+	<?php if(stripos($_SERVER['SCRIPT_NAME'], 'gallery.php')){
+		include_once __DIR__.'/gallery.php';
+	}else{
+		include_once __DIR__.'/index.php';
+	} ?>
+	<?php include_once __DIR__.'/lang_modal.php' ?>
+</main>
+<?php include_once __DIR__.'/footer.php'; ?>
 </body>
 </html>
