b20de403f9f2c82891ae9de75bfe1a734f5e3120
viiper94 <viiper94@gmail.com>
2018-05-28 12:30:39 +0300

    using local font file instead of remote

--- views/header.php
+++ views/header.php
@@ -2,12 +2,11 @@
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link type="text/css" rel="stylesheet" href="assets/mdc/css/material-components-web.min.css" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="assets/semanticui/semanticui.min.css" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="assets/semanticui/semantic-custom.min.css" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="assets/materialize/css/materialize.min.css" media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="assets/css/style-v1.4.2.min.css" media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="assets/css/style-v1.4.3.min.css" media="screen,projection"/>
 <!-- Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-99570317-2"></script>
 <script>
