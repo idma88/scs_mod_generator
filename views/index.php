0266f3a3fa18ae3902b0fe6c8abba6b9d2c36ee4
viiper94 <viiper94@gmail.com>
2018-05-28 12:31:10 +0300

    add notranslate classes to material icons

--- views/index.php
+++ views/index.php
@@ -3,7 +3,7 @@
 		<section class="row">
 			<div class="download-row">
 				<a href="/download/<?= $_GET['d'] ?>.scs" class="mdc-button mdc-button--raised mdc-ripple large-btn left">
-					<i class="material-icons mdc-button__icon">file_download</i>
+					<i class="material-icons notranslate mdc-button__icon">file_download</i>
 					<?= I18n::t('download_mod') ?>
 				</a>
 				<h6><?= $_GET['d'] ?>.scs</h6>
@@ -137,7 +137,7 @@
 				<section class="advanced row" style="margin-bottom: 0;">
 					<ul class="collapsible z-depth-0 col s12">
 						<li>
-							<div class="collapsible-header grey-text"><i class="material-icons">arrow_drop_down</i><?= I18n::t('advanced') ?></div>
+							<div class="collapsible-header grey-text"><i class="material-icons notranslate">arrow_drop_down</i><?= I18n::t('advanced') ?></div>
 							<div class="collapsible-body">
 								<label><?= I18n::t('image_upload') ?></label>
 								<div class="file-field input-field mdc-button mdc-button--raised">
