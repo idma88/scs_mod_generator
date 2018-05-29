22c231e3c9bb34b35d1ca8da10d359e2384ec6c8
viiper94 <viiper94@gmail.com>
2018-05-29 18:54:26 +0300

    improve choosing language

--- classes/I18n.php
+++ classes/I18n.php
@@ -14,11 +14,23 @@ class I18n{
 	}
 
 	public static function getUserLanguage(){
+		GLOBAL $languages;
 		if(!isset($_COOKIE['lang'])){
-			$lang = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE'])[0];
-			$lang = explode('-', $lang)[0];
-			$ru = ['ru', 'uk', 'be', 'kk', 'mo'];
-			$lang = in_array($lang, $ru) ? 'ru' : 'en';
+			$user_lang = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE'])[0];
+			$user_lang = explode('-', $user_lang)[0];
+			$locales_per_lang = [
+				'ru' => ['ru', 'uk', 'be', 'kk', 'mo', 'ky', 'lv', 'lt', 'et', 'ka', 'az', 'hy', 'uz', 'tk'],
+				'de' => ['de', 'da', 'nl'],
+				'fr' => ['fr', ''],
+				'es' => ['es', 'pt'],
+				'pt' => ['pt', 'es'],
+			];
+			if(!key_exists($user_lang, $languages)){
+				$lang = 'en';
+				foreach($locales_per_lang as $language => $locales){
+					if(in_array($user_lang, $locales)) $lang = $language;
+				}
+			}else $lang = $user_lang;
 		}else{
 			$lang = $_COOKIE['lang'];
 		}
