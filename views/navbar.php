324078763bbad67f3c85662bbc07759bd1d39035
viiper94 <viiper94@gmail.com>
2018-07-23 11:22:41 +0300

    add logo

--- views/navbar.php
+++ views/navbar.php
@@ -4,7 +4,7 @@ $_SERVER['SCRIPT_NAME'] !== '/gallery.php' ? : $game = null; ?>
 <div class="navbar-fixed">
 	<nav class="blue-grey darken-3">
 		<div class="nav-wrapper">
-			<a href="/" class="brand-logo left" style="font-weight: 500; text-transform: uppercase; margin-left: 15px;"><?= I18n::t('head_title') ?></a>
+			<a href="/" class="brand-logo left" style="font-weight: 500; text-transform: uppercase; margin-left: 35px;"><?= I18n::t('head_title') ?></a>
 			<ul id="nav-mobile" class="right">
 				<li<?php if($game == 'ets2'): ?> class="active"<?php endif ?>><a href="/"><?= I18n::t('ets2') ?></a></li>
 				<li<?php if($game == 'ats'): ?> class="active"<?php endif ?>><a href="/?game=ats"><?= I18n::t('ats') ?></a></li>
